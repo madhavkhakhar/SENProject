b8e502dc62d4070b8a5d5df487154f3661efcc3e
Saurabh <srp201201051@gmail.com>
2015-03-26 23:52:33 +0530

    Changed to external directory path for images and created Util to play and load image

---
+++ GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/Util.java
@@ -0,0 +1,32 @@
+package com.codebenders.gujaratimitra;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+/**
+ * Created by saurabh on 26/3/15.
+ */
+public class Util {
+
+    public static MediaPlayer mediaPlayer;
+
+    public static void playMediaFromPath(String path) {
+        mediaPlayer.reset();
+        try {
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setImageFromPath(ImageView imView, String path) {
+        Bitmap b = BitmapFactory.decodeFile(path);
+        imView.setImageBitmap(b);
+    }
+}
