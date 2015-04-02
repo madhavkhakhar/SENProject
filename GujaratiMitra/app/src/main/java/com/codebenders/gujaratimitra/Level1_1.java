c9659723739ed3d0d1578595094c89f8419aea05
Saurabh <srp201201051@gmail.com>
2015-04-02 18:23:22 +0530

    Level 1_1 implementation

--- GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/Level1_1.java
+++ GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/Level1_1.java
@@ -1,17 +1,20 @@
 package com.codebenders.gujaratimitra;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class Level1_1 extends ActionBarActivity {
@@ -22,21 +25,36 @@ public class Level1_1 extends ActionBarActivity {
     private ImageView leftArrow;
     private ImageView rightArrow;
     private ImageView queImage;
-    private Random rn;
+    private ImageView queStatement;
+    private boolean sleeping;
+    private ArrayList<Integer> queImageIndex;
+    private int currentQueIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1_1);
+
+        sleeping = false;
+        queImageIndex = new ArrayList<Integer>();
+        for (int i = 1; i <= 16; i++) {
+            queImageIndex.add(i);
+        }
+        Collections.shuffle(queImageIndex);
+
         mPager = (ViewPager) findViewById(R.id.pager);
         mAdapter = new SimplePagerAdapter();
         mPager.setAdapter(mAdapter);
         mPager.setOffscreenPageLimit(0);
+
+        queStatement = (ImageView) findViewById(R.id.que_statement);
+        Util.setImageFromPath(queStatement, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_que.png");
+
         queImage = (ImageView) findViewById(R.id.que_image);
-        rn = new Random();
-        int pos = 1+rn.nextInt(16);
-        Util.setImageFromPath(queImage, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_" + pos + ".png");
-        queImage.setTag(pos);
+        Util.setImageFromPath(queImage, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_" + queImageIndex.get(currentQueIndex) + ".png");
+        queImage.setTag(queImageIndex.get(currentQueIndex));
+        currentQueIndex = (currentQueIndex+1)%16;
+
         leftArrow = (ImageView) findViewById(R.id.imageView);
         rightArrow = (ImageView) findViewById(R.id.imageView1);
         leftArrow.setOnClickListener(new View.OnClickListener() {
@@ -99,11 +117,9 @@ public class Level1_1 extends ActionBarActivity {
             final ImageView v4 = (ImageView) view.findViewById(R.id.img4);
 
             Util.setImageFromPath(v1, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+((4*position)+1)+".png");
-            Util.setImageFromPath(v3, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+((4*position)+2)+".png");
-            Util.setImageFromPath(v2, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+((4*position)+3)+".png");
+            Util.setImageFromPath(v2, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+((4*position)+2)+".png");
+            Util.setImageFromPath(v3, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+((4*position)+3)+".png");
             Util.setImageFromPath(v4, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+((4*position)+4)+".png");
-            //v1.setTag(Environment.getExternalStorageDirectory() + "/GujaratiMitra/l4/1/aud_" + Integer.toString(2 * position) + ".mp3");
-            //v3.setTag(Environment.getExternalStorageDirectory() + "/GujaratiMitra/l4/1/aud_" + Integer.toString((2 * position) + 1) + ".mp3");
 
             v1.setTag(((4*position)+1));
             v2.setTag(((4*position)+2));
@@ -113,44 +129,36 @@ public class Level1_1 extends ActionBarActivity {
             v1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (queImage.getTag().equals(v1.getTag())) {
-                        int pos = 1+rn.nextInt(16);
-                        System.out.println(">>>>>>>pos"+pos);
-                        Util.setImageFromPath(queImage, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+pos+".png");
-                        queImage.setTag(pos);
+                    if (!sleeping) {
+                        System.out.println("clicked!");
+                        loadNextImage(v1.getTag().toString());
                     }
                 }
             });
             v2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (queImage.getTag().equals(v2.getTag())) {
-                        int pos = 1+rn.nextInt(16);
-                        System.out.println(">>>>>>>pos"+pos);
-                        Util.setImageFromPath(queImage, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+pos+".png");
-                        queImage.setTag(pos);
+                    if (!sleeping) {
+                        System.out.println("clicked!!");
+                        loadNextImage(v2.getTag().toString());
                     }
                 }
             });
             v3.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (queImage.getTag().equals(v3.getTag())) {
-                        int pos = 1+rn.nextInt(16);
-                        System.out.println(">>>>>>>pos"+pos);
-                        Util.setImageFromPath(queImage, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+pos+".png");
-                        queImage.setTag(pos);
+                    if (!sleeping) {
+                        System.out.println("clicked!!!");
+                        loadNextImage(v3.getTag().toString());
                     }
                 }
             });
             v4.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (queImage.getTag().equals(v4.getTag())) {
-                        int pos = 1+rn.nextInt(16);
-                        System.out.println(">>>>>>>pos"+pos);
-                        Util.setImageFromPath(queImage, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_"+pos+".png");
-                        queImage.setTag(pos);
+                    if (!sleeping) {
+                        System.out.println("clicked!!!!");
+                        loadNextImage(v4.getTag().toString());
                     }
                 }
             });
@@ -161,6 +169,60 @@ public class Level1_1 extends ActionBarActivity {
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
+        }
+
+        public void loadNextImage (final String pos) {
+            new Thread() {
+                @Override
+                public void run() {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Vibrator v = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+                            if(queImage.getTag().toString().equals(pos)) {
+                                final Toast toast = Toast.makeText(getApplicationContext(), "CORRECT", Toast.LENGTH_SHORT);
+                                toast.show();
+
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        toast.cancel();
+                                    }
+                                }, 500);
+                            } else {
+                                final Toast toast = Toast.makeText(getApplicationContext(), "WRONG", Toast.LENGTH_SHORT);
+                                toast.show();
+
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        toast.cancel();
+                                    }
+                                }, 500);
+
+                                v.vibrate(500);
+                            }
+                        }
+                    });
+                    try {
+                        sleeping = true;
+                        Thread.sleep(1000);
+                        sleeping = false;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Util.setImageFromPath(queImage, Environment.getExternalStorageDirectory() + "/GujaratiMitra/l1/1/img_" + queImageIndex.get(currentQueIndex) + ".png");
+                            queImage.setTag(queImageIndex.get(currentQueIndex));
+                            currentQueIndex = (currentQueIndex+1)%16;
+                        }
+                    });
+                }
+            }.start();
         }
     }
 }
