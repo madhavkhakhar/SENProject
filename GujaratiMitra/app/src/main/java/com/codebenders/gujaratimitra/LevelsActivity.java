f403cab2a64d1137ed1c95e687d2e5f0a7b7bb61
Raxit <chauhanraxit@gmail.com>
2015-04-03 18:56:16 +0530

    Level 1_1 done

--- GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/LevelsActivity.java
+++ GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/LevelsActivity.java
@@ -29,6 +29,8 @@ public class LevelsActivity extends ActionBarActivity {
     private static int NUM_PAGES = 3;
     ViewPager viewPager;
     private int levelNo=0;
+    ListView listView;
+    ArrayList<String> listItems= new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +40,7 @@ public class LevelsActivity extends ActionBarActivity {
         viewPager.setAdapter(adapter);
         viewPager.setOffscreenPageLimit(NUM_PAGES);
     }
-    private class CustomPagerAdapter extends PagerAdapter{
+    private class CustomPagerAdapter extends PagerAdapter {
 
         @Override
         public int getCount() {
@@ -53,24 +55,24 @@ public class LevelsActivity extends ActionBarActivity {
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             View view = getLayoutInflater().inflate(R.layout.home_screen_page_layout, container, false);
-            ImageView level1,level2,level3,level4,level5,level6,level7;
-            LinearLayout layout = (LinearLayout)view.findViewById(R.id.main_layout);
-            level1 = (ImageView)view.findViewById(R.id.level1);
-            level2 = (ImageView)view.findViewById(R.id.level2);
-            level3 = (ImageView)view.findViewById(R.id.level3);
-            level4 = (ImageView)view.findViewById(R.id.level4);
-            level5 = (ImageView)view.findViewById(R.id.level5);
-            level6 = (ImageView)view.findViewById(R.id.level6);
-            level7 = (ImageView)view.findViewById(R.id.level7);
-            level1.setTag(""+position);
-            level2.setTag(""+position);
-            level3.setTag(""+position);
-            level4.setTag(""+position);
-            level5.setTag(""+position);
-            level6.setTag(""+position);
-            level7.setTag(""+position);
-           // level1.setTag("");
-            if(position==0){
+            ImageView level1, level2, level3, level4, level5, level6, level7;
+            LinearLayout layout = (LinearLayout) view.findViewById(R.id.main_layout);
+            level1 = (ImageView) view.findViewById(R.id.level1);
+            level2 = (ImageView) view.findViewById(R.id.level2);
+            level3 = (ImageView) view.findViewById(R.id.level3);
+            level4 = (ImageView) view.findViewById(R.id.level4);
+            level5 = (ImageView) view.findViewById(R.id.level5);
+            level6 = (ImageView) view.findViewById(R.id.level6);
+            level7 = (ImageView) view.findViewById(R.id.level7);
+            level1.setTag("" + position);
+            level2.setTag("" + position);
+            level3.setTag("" + position);
+            level4.setTag("" + position);
+            level5.setTag("" + position);
+            level6.setTag("" + position);
+            level7.setTag("" + position);
+            // level1.setTag("");
+            if (position == 0) {
                 level1.setImageResource(R.drawable.home_s1_l1);
                 level2.setImageResource(R.drawable.home_s1_l2);
                 level3.setImageResource(R.drawable.home_s1_l3);
@@ -78,16 +80,15 @@ public class LevelsActivity extends ActionBarActivity {
                 level5.setImageResource(R.drawable.home_s1_l5);
                 level6.setImageResource(R.drawable.home_s1_l6);
                 level7.setImageResource(R.drawable.home_s1_l7);
-                level1.setTag(""+position);
-                level2.setTag(""+position);
-                level3.setTag(""+position);
-                level4.setTag(""+position);
-                level5.setTag(""+position);
-                level6.setTag(""+position);
-                level7.setTag(""+position);
+                level1.setTag("" + position);
+                level2.setTag("" + position);
+                level3.setTag("" + position);
+                level4.setTag("" + position);
+                level5.setTag("" + position);
+                level6.setTag("" + position);
+                level7.setTag("" + position);
                 layout.setBackgroundResource(R.drawable.home_bg_s1);
-            }
-            else if(position==1) {
+            } else if (position == 1) {
                 level1.setImageResource(R.drawable.home_s2_l1);
                 level2.setImageResource(R.drawable.home_s2_l2);
                 level3.setImageResource(R.drawable.home_s2_l3);
@@ -96,9 +97,7 @@ public class LevelsActivity extends ActionBarActivity {
                 level6.setImageResource(R.drawable.home_s2_l6);
                 level7.setImageResource(R.drawable.home_s2_l7);
                 layout.setBackgroundResource(R.drawable.home_bg_s2);
-            }
-
-            else if(position==2){
+            } else if (position == 2) {
                 level1.setImageResource(R.drawable.home_s3_l1);
                 level2.setImageResource(R.drawable.home_s3_l2);
                 level3.setImageResource(R.drawable.home_s3_l3);
@@ -116,11 +115,11 @@ public class LevelsActivity extends ActionBarActivity {
                         Intent i = new Intent(LevelsActivity.this,Level3_1.class);
                         startActivity(i);
                     }*/
-                    levelNo=1;
-                    Intent i = new Intent(LevelsActivity.this,SubLevelsActivity.class);
-                    i.putExtra("Level",levelNo);
+                    levelNo = 1;
+                    Intent i = new Intent(LevelsActivity.this, SubLevelsActivity.class);
+                    i.putExtra("Level", levelNo);
                     startActivity(i);
-                    }
+                }
 
             });
 
@@ -128,9 +127,9 @@ public class LevelsActivity extends ActionBarActivity {
                 @Override
                 public void onClick(View v) {
                     //Intent intent = new Intent(LevelsActivity.this,Lev)
-                    Intent i = new Intent(LevelsActivity.this,SubLevelsActivity.class);
-                    levelNo=2;
-                    i.putExtra("Level",levelNo);
+                    Intent i = new Intent(LevelsActivity.this, SubLevelsActivity.class);
+                    levelNo = 2;
+                    i.putExtra("Level", levelNo);
                     startActivity(i);
                 }
             });
@@ -139,8 +138,9 @@ public class LevelsActivity extends ActionBarActivity {
                 @Override
                 public void onClick(View v) {
                     //Intent intent = new Intent(LevelsActivity.this,Lev)
-                    Intent i = new Intent(LevelsActivity.this,SubLevelsActivity.class);levelNo=3;
-                    i.putExtra("Level",levelNo);
+                    Intent i = new Intent(LevelsActivity.this, SubLevelsActivity.class);
+                    levelNo = 3;
+                    i.putExtra("Level", levelNo);
                     startActivity(i);
                 }
             });
@@ -149,9 +149,9 @@ public class LevelsActivity extends ActionBarActivity {
                 @Override
                 public void onClick(View v) {
                     //Intent intent = new Intent(LevelsActivity.this,Lev)
-                    Intent i = new Intent(LevelsActivity.this,SubLevelsActivity.class);
-                    levelNo=4;
-                    i.putExtra("Level",levelNo);
+                    Intent i = new Intent(LevelsActivity.this, SubLevelsActivity.class);
+                    levelNo = 4;
+                    i.putExtra("Level", levelNo);
                     startActivity(i);
                 }
             });
@@ -160,9 +160,9 @@ public class LevelsActivity extends ActionBarActivity {
                 @Override
                 public void onClick(View v) {
                     //Intent intent = new Intent(LevelsActivity.this,Lev)
-                    Intent i = new Intent(LevelsActivity.this,SubLevelsActivity.class);
-                    levelNo=5;
-                    i.putExtra("Level",levelNo);
+                    Intent i = new Intent(LevelsActivity.this, SubLevelsActivity.class);
+                    levelNo = 5;
+                    i.putExtra("Level", levelNo);
                     startActivity(i);
                 }
             });
@@ -170,9 +170,9 @@ public class LevelsActivity extends ActionBarActivity {
                 @Override
                 public void onClick(View v) {
                     //Intent intent = new Intent(LevelsActivity.this,Lev)
-                    Intent i = new Intent(LevelsActivity.this,SubLevelsActivity.class);
-                    levelNo=6;
-                    i.putExtra("Level",levelNo);
+                    Intent i = new Intent(LevelsActivity.this, SubLevelsActivity.class);
+                    levelNo = 6;
+                    i.putExtra("Level", levelNo);
                     startActivity(i);
                 }
             });
@@ -181,14 +181,12 @@ public class LevelsActivity extends ActionBarActivity {
                 @Override
                 public void onClick(View v) {
                     //Intent intent = new Intent(LevelsActivity.this,Lev)
-                    Intent i = new Intent(LevelsActivity.this,SubLevelsActivity.class);
-                    levelNo=7;
-                    i.putExtra("Level",levelNo);
+                    Intent i = new Intent(LevelsActivity.this, SubLevelsActivity.class);
+                    levelNo = 7;
+                    i.putExtra("Level", levelNo);
                     startActivity(i);
                 }
             });
-
-
             container.addView(view);
             return view;
         }
