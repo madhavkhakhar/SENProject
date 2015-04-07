833abaad960640ed52137ccc7ae4f55542682aa5
nihartrivedi810 <nihar.trivedi888@gmail.com>
2015-04-07 16:26:50 +0530

    basic database queries and function

--- GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/database/AppDB.java
+++ GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/database/AppDB.java
@@ -1,88 +1,141 @@
 package com.codebenders.gujaratimitra.database;
 
-import java.util.ArrayList;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.content.Context;
 import android.database.Cursor;
 
 import com.codebenders.gujaratimitra.profile.Student;
 
+import java.util.ArrayList;
+
 public class AppDB extends DBConnect {
-	
-	public AppDB(Context context) {
-		super(context);
-	}
+
+    public AppDB(Context context) {
+        super(context);
+    }
 
 
-	public void insertStudent(Student student) {
-		String query;
+    public void insertStudent(Student student) {
+        String query;
 
         query = String.format(ISql.INSERT_STUDENT, student.getRoll(),
-                student.getStandard(),student.getFirstName(),student.getLastName());
+                student.getStandard(), student.getFirstName(), student.getLastName());
 
         execNonQuery(query);
-	}
- 
-	/**
-	 * To remove Student
-	 */
-	public void removeStudent(int rollNo)
-	{
-		if(rollNo>0)
-		{
-			String sqlRemoveRegCard = String.format(ISql.REMOVE_STUDENT, rollNo);
-			execNonQuery(sqlRemoveRegCard);
-		}
-	}
-	
-	/**
-	 * To count no. of Students
-	 * @return
-	 */
-	public int countStudent()
-	{
-		Cursor cursor = execQuery(ISql.COUNT_STUDENT);
-		int cntCards = 0;
-		
-		if(cursor!=null && cursor.getCount()>0) {
-			cursor.moveToNext();
-			cntCards = Integer.parseInt(cursor.getString(0));
-		}
-		
-		if(cursor!= null) {
-			cursor.close();
-		}
-			
-		return cntCards;
-	}
-	
-	/**
-	 * To get all the Students
-	 * @return
-	 */
-	public ArrayList<Student> getStudents() {
-		Cursor cursor = execQuery(ISql.GET_STUDENT);
+    }
 
-		ArrayList<Student> listStudent = new ArrayList<Student>();
-		Student student;
-		if (cursor != null && cursor.getCount() > 0) {
+    /**
+     * To remove Student
+     */
+    public void removeStudent(int rollNo) {
+        if (rollNo > 0) {
+            String sqlRemoveRegCard = String.format(ISql.REMOVE_STUDENT, rollNo);
+            execNonQuery(sqlRemoveRegCard);
+        }
+    }
 
-			if (cursor.moveToNext()) {
+    /**
+     * To count no. of Students
+     *
+     * @return
+     */
+    public int countStudent() {
+        Cursor cursor = execQuery(ISql.COUNT_STUDENT);
+        int cntCards = 0;
 
-				do {
-                        student = new Student(cursor.getInt(cursor.getColumnIndex("roll_no")),cursor.getInt(cursor.getColumnIndex("standard")),cursor.getString(cursor.getColumnIndex("first_name")),cursor.getString(cursor.getColumnIndex("last_name")));
-						listStudent.add(student);
-				} while (cursor.moveToNext());
-			}
-		}
+        if (cursor != null && cursor.getCount() > 0) {
+            cursor.moveToNext();
+            cntCards = Integer.parseInt(cursor.getString(0));
+        }
 
-		if (cursor != null) {
-			cursor.close();
-		}
+        if (cursor != null) {
+            cursor.close();
+        }
 
-		return listStudent;
-	}
+        return cntCards;
+    }
+
+    /**
+     * To get all the Students
+     *
+     * @return
+     */
+    public ArrayList<Student> getStudents() {
+        Cursor cursor = execQuery(ISql.GET_STUDENT);
+
+        ArrayList<Student> listStudent = new ArrayList<Student>();
+        Student student;
+        if (cursor != null && cursor.getCount() > 0) {
+
+            if (cursor.moveToNext()) {
+
+                do {
+                    student = new Student(cursor.getInt(cursor.getColumnIndex("roll_no")), cursor.getInt(cursor.getColumnIndex("standard")), cursor.getString(cursor.getColumnIndex("first_name")), cursor.getString(cursor.getColumnIndex("last_name")));
+                    listStudent.add(student);
+                } while (cursor.moveToNext());
+            }
+        }
+
+        if (cursor != null) {
+            cursor.close();
+        }
+
+        return listStudent;
+    }
+
+    public void addSubLevelScore(int levelNo, int subLevelNo, int score, int studentId) {
+        String query = String.format(ISql.ADD_SUB_LEVEL_SCORE, studentId,
+                subLevelNo, levelNo, score);
+        execNonQuery(query);
+    }
+
+    public int getSubLevelScore(int studentId, int subLevelNo) {
+        String query = String.format(ISql.GET_SUB_LEVEL_SCORE, studentId,
+                subLevelNo);
+        Cursor cursor = execQuery(query);
+        int score = 0;
+        if (cursor != null && cursor.getCount() > 0) {
+
+            if (cursor.moveToNext()) {
+                score = cursor.getInt(0);
+            }
+        }
+        return score;
+    }
+
+    public int getLevelScore(int studentId, int levelNo) {
+        String query = String.format(ISql.GET_SUB_LEVEL_SCORE, levelNo, studentId);
+        Cursor cursor = execQuery(query);
+        int score = 0;
+        if (cursor != null && cursor.getCount() > 0) {
+
+            if (cursor.moveToNext()) {
+                score = cursor.getInt(0);
+            }
+        }
+        return score;
+    }
+    public int getLastLevelUnlocked(int studentId){
+        String query = String.format(ISql.GET_LAST_LEVEL_UNLOCKED, studentId);
+        Cursor cursor = execQuery(query);
+        int level = 1;
+        if (cursor != null && cursor.getCount() > 0) {
+
+            if (cursor.moveToNext()) {
+                level = cursor.getInt(0);
+            }
+        }
+        return level;
+    }
+    public int getLastSubLevelUnlocked(int studentId){
+        String query = String.format(ISql.GET_LAST_LEVEL_UNLOCKED, studentId);
+        Cursor cursor = execQuery(query);
+        int subLevel = 1;
+        if (cursor != null && cursor.getCount() > 0) {
+
+            if (cursor.moveToNext()) {
+                subLevel = cursor.getInt(0);
+            }
+        }
+        return subLevel;
+    }
 }
