173e33b57248f32cfb47e267759cf097a99f5ec2
nihartrivedi810 <nihar.trivedi888@gmail.com>
2015-04-18 19:27:21 +0530

    added intent flags in profiles activity

--- GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/profile/ProfileActivity.java
+++ GujaratiMitra/app/src/main/java/com/codebenders/gujaratimitra/profile/ProfileActivity.java
@@ -2,22 +2,23 @@ package com.codebenders.gujaratimitra.profile;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.DisplayMetrics;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
-import static com.codebenders.gujaratimitra.Util.*;
 
-import com.codebenders.gujaratimitra.AppPreferences;
 import com.codebenders.gujaratimitra.LevelsActivity;
 import com.codebenders.gujaratimitra.R;
 
 import java.util.ArrayList;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static com.codebenders.gujaratimitra.Util.appDB;
+import static com.codebenders.gujaratimitra.Util.prefs;
 
 public class ProfileActivity extends ActionBarActivity {
 
@@ -25,14 +26,15 @@ public class ProfileActivity extends ActionBarActivity {
     ListView listView;
     ArrayList<Student> students;
     ProfileListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         students = appDB.getStudents();
-        addProfile = (Button)findViewById(R.id.add_profile_button);
-        listView = (ListView)findViewById(R.id.listView);
-        adapter = new ProfileListAdapter(ProfileActivity.this,R.layout.profile_list_view_item,students);
+        addProfile = (Button) findViewById(R.id.add_profile_button);
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new ProfileListAdapter(ProfileActivity.this, R.layout.profile_list_view_item, students);
         listView.setAdapter(adapter);
         addProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,9 +44,7 @@ public class ProfileActivity extends ActionBarActivity {
                 int width = metrics.widthPixels;
                 int height = metrics.heightPixels;
                 CreateProfileDialog createProfileDialog = new CreateProfileDialog(
-                        ProfileActivity.this)
-
-                        ;
+                        ProfileActivity.this);
                 createProfileDialog.setCanceledOnTouchOutside(true);
                 createProfileDialog.show();
                 createProfileDialog.getWindow().setLayout((4 * width) / 7,
@@ -53,7 +53,7 @@ public class ProfileActivity extends ActionBarActivity {
                     @Override
                     public void onDismiss(DialogInterface dialog) {
                         students = appDB.getStudents();
-                        adapter = new ProfileListAdapter(ProfileActivity.this,R.layout.profile_list_view_item,students);
+                        adapter = new ProfileListAdapter(ProfileActivity.this, R.layout.profile_list_view_item, students);
                         listView.setAdapter(adapter);
                     }
                 });
@@ -64,7 +64,8 @@ public class ProfileActivity extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 prefs.saveStudentId(students.get(position).getId());
-                startActivity(new Intent(ProfileActivity.this, LevelsActivity.class));
+                startActivity(new Intent(ProfileActivity.this, LevelsActivity.class).addFlags(FLAG_ACTIVITY_CLEAR_TASK | FLAG_ACTIVITY_NEW_TASK));
+
                 finish();
             }
         });
